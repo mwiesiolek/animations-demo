510d66b82d0b4d80967e448f24f679c74386df0e
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-02 22:07:14 +0100

    reusable animation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,19 +1,12 @@
-import { Component } from '@angular/core';
-import {trigger, state, transition, style, animate} from "@angular/animations";
+import {Component} from "@angular/core";
+import {fade} from "../animations";
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
-    trigger('fade', [
-
-      state('void', style({ opacity: 0 })),
-
-      transition(':enter, :leave', [
-        animate(2000)
-      ])
-    ])
+    fade
   ]
 })
 export class TodosComponent {
