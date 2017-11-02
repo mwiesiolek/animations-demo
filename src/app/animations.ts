18698733f355f327e93b80745dac8ddcebf30a16
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-02 22:30:07 +0100

    key frames

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import {trigger, state, transition, style, animate} from "@angular/animations";
+import {trigger, state, transition, style, animate, keyframes} from "@angular/animations";
 
 export let fade = trigger('fade', [
   state('void', style({opacity: 0})),
@@ -14,6 +14,20 @@ export let slide = trigger('slide', [
   ]),
 
   transition(':leave', [
+    //animate('0.5s ease-in/ease-out/ease-in-out')
     animate('0.5s cubic-bezier(.61, .29, .07, 1.02)', style({ transform: 'translateX(-100%)' }))
+  ])
+]);
+
+export let slideWithKeyFrame = trigger('slideWithKeyFrame', [
+  state('void', style({ transform: 'translateX(-20px)' })),
+  transition(':enter', [
+    animate(500)
+  ]),
+  transition(':leave', [
+    animate('0.5s ease-out', keyframes([
+      style({ offset: .2, opacity: 1, transform: 'translateX(20px)' }),
+      style({ offset: 1, opacity: 0, transform: 'translateX(-100%)' })
+    ]))
   ])
 ]);
